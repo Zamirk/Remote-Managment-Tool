a1372e0aa4ed24ff5ffcf5fb29e771753266e795
Samir Kahvedzic <samir.kahvedzic1@gmail.com>
2016-12-02 12:40:55 +0000

    CPU Live graph fixed, MainManamentScreen started

--- RAT/RAT/1View/Desktop/2Performance/CPUScreen.cs
+++ RAT/RAT/1View/Desktop/2Performance/CPUScreen.cs
@@ -8,12 +8,18 @@ using ServerMonitor;
 using Syncfusion.SfChart.XForms;
 using Syncfusion.SfDataGrid.XForms;
 using Xamarin.Forms;
+using ChartDataPoint = Syncfusion.SfChart.XForms.ChartDataPoint;
+using FastLineSeries = Syncfusion.SfChart.XForms.FastLineSeries;
+using NumericalAxis = Syncfusion.SfChart.XForms.NumericalAxis;
+using SfChart = Syncfusion.SfChart.XForms.SfChart;
 
 namespace RAT._1View.Desktop
 {
 	public class CPUScreen : Grid
 	{
-	    private int i = 40;
+        private bool test = true;
+        Random r = new Random();
+        private int sizeOfGraph = 100;
 	    private SfChart chart;
 	    private List<SfChart> myCharts;
         public CPUScreen()
@@ -34,68 +40,49 @@ namespace RAT._1View.Desktop
                 myCharts.Add(new SfChart());
 
                 myCharts[i].Series.Add(new FastLineSeries());
-                myCharts[i].Series.Add(new FastLineSeries());
 
                 myCharts[i].VerticalOptions = LayoutOptions.Start;
                 myCharts[i].HorizontalOptions = LayoutOptions.Start;
                 
                 Children.Add(myCharts[i], 0,i);
-                myCharts[i].Series[1].EnableTooltip = true;
                 aaa = getData();
+
                 myCharts[i].Series[0].ItemsSource = aaa;
-                myCharts[i].Series[1].ItemsSource = aaa;
 
-                //myCharts[i].Series[0].ItemsSource = aaa;
-                //myCharts[i].Series[1].ItemsSource = getData();
+                myCharts[0].PrimaryAxis = new NumericalAxis();
+                myCharts[0].SecondaryAxis = new NumericalAxis();
 
-
-
-                // myCharts[i].Series[1].Label = "Test Label"
-
-                //column.ItemsSource = viewModel.ColumnData;
-                //column.XBindingPath = "Name";
-                //column.YBindingPath = "Value";
-                //myCharts[i].Series[0].EnableAnimation = true;
-                // myCharts[i].Series[0].AnimationDuration = 2;
-                ////myCharts[i].Series[0].
-                /// 
-                /// 
-                /// 
-                //myCharts[0].PrimaryAxis = new NumericalAxis();
-                //myCharts[0].SecondaryAxis = new NumericalAxis();
-
-                //ViewModel viewModel = myCharts[i].BindingContext as ViewModel;
-                ////viewModel.LoadData1();
-                //myCharts[0].Series[0].SetBinding(FastLineSeries.XAxisProperty, "Value");
-                //myCharts[0].Series[0].ItemsSource = viewModel.liveData1;
-
-                //// (myCharts[i].SecondaryAxis as NumericalAxis).Maximum = 100;
-                ////     (myCharts[i].SecondaryAxis as NumericalAxis).Minimum = 0;
-
-                //           (myCharts[i].PrimaryAxis as NumericalAxis).Maximum = 100;
-                //      (myCharts[i].PrimaryAxis as NumericalAxis).Minimum = 0;
+                (myCharts[i].SecondaryAxis as NumericalAxis).Maximum = 100;
+                (myCharts[i].SecondaryAxis as NumericalAxis).Minimum = 0;
+                    //(myCharts[i].PrimaryAxis as NumericalAxis).Maximum = 100;
+                    //(myCharts[i].PrimaryAxis as NumericalAxis).Minimum = 0;
             }
 
 
-
-
-
-
-            Device.StartTimer(TimeSpan.FromMilliseconds(1000), () =>
+        
+            Device.StartTimer(TimeSpan.FromMilliseconds(500), () =>
              {
-             AddData();
-
-                  return true;
-               });
+                AddData();
+                return true;
+             });
         }
 
 	    public ObservableCollection<ChartDataPoint> aaa { get; set; }
         private ObservableCollection<ChartDataPoint> getData()
         {
             ObservableCollection<ChartDataPoint> datas = new ObservableCollection<ChartDataPoint>();
-            for (int i = 0; i < 40; i++)
+            for (int i = 0; i < sizeOfGraph; i++)
             {
-            datas.Add(new ChartDataPoint(i+1, 34));
+                if (test)
+                {
+                    datas.Add(new ChartDataPoint(i + 1, 55));
+                    test = false;
+                }
+                else
+                {
+                    datas.Add(new ChartDataPoint(i + 1, 45));
+                    test = true;
+                }
 
             }
             return datas;
@@ -103,22 +90,26 @@ namespace RAT._1View.Desktop
 
         private bool AddData()
         {
-            i += 1;
-            int a = r.Next(10, 90);
-            int b = r.Next(10, 90);
-            int c = r.Next(10, 90);
-            if (a < 10)
-            {
-                a = 1;
-            }
-
+            sizeOfGraph += 1;
             aaa.RemoveAt(0);
-            aaa.Add(new ChartDataPoint(i, a * .5));
-            (myCharts[0].Series[0].ItemsSource as ObservableCollection<ChartDataPoint>).RemoveAt(0);
-            (myCharts[0].Series[1].ItemsSource as ObservableCollection<ChartDataPoint>).Add(new ChartDataPoint(i, a*.5));
+            aaa.Add(new ChartDataPoint(sizeOfGraph, r.Next(10, 90)));
 
             return true;
         }
-        Random r = new Random();
     }
 }
+//myCharts[i].Series[0].ItemsSource = aaa;
+//myCharts[i].Series[1].ItemsSource = getData();
+
+// myCharts[i].Series[1].Label = "Test Label"
+
+//column.ItemsSource = viewModel.ColumnData;
+//column.XBindingPath = "Name";
+//column.YBindingPath = "Value";
+//myCharts[i].Series[0].EnableAnimation = true;
+// myCharts[i].Series[0].AnimationDuration = 2;
+////myCharts[i].Series[0].
+//ViewModel viewModel = myCharts[i].BindingContext as ViewModel;
+////viewModel.LoadData1();
+//myCharts[0].Series[0].SetBinding(FastLineSeries.XAxisProperty, "Value");
+//myCharts[0].Series[0].ItemsSource = viewModel.liveData1;
--- RAT/RAT/1View/Desktop/2Performance/DiskScreen.cs
+++ RAT/RAT/1View/Desktop/2Performance/DiskScreen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using SampleBrowser;
 using ServerMonitor;
 using Syncfusion.SfChart.XForms;
@@ -11,30 +12,117 @@ namespace RAT._1View.Desktop
         public DiskScreen()
         {
             //Chart 1
-            VerticalOptions = LayoutOptions.FillAndExpand;
-            RowDefinitions.Add(new RowDefinition {Height = GridLength.Auto});
-            ColumnDefinitions.Add(new ColumnDefinition {Width = GridLength.Auto});
+            //VerticalOptions = LayoutOptions.FillAndExpand;
+            //RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto});
+            //ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto});
+            //ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            //ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            //ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
 
-            ContentView myV = new ContentView()
+            int size = 150;
+
+            ContentView myV1 = new ContentView()
             {
             BackgroundColor = Color.Red,
-            VerticalOptions = LayoutOptions.CenterAndExpand,
+            VerticalOptions = LayoutOptions.StartAndExpand,
             HorizontalOptions = LayoutOptions.CenterAndExpand,
-            WidthRequest = 200,
-            HeightRequest = 200
-        };
+            WidthRequest = size,
+            HeightRequest = size
+            };
 
             ContentView myV2 = new ContentView()
             {
                 BackgroundColor = Color.Green,
-                VerticalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.StartAndExpand,
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
-                WidthRequest = 200,
-                HeightRequest = 200
+                WidthRequest = size,
+                HeightRequest = size
             };
 
-            Children.Add(myV2,0,1);
+            ContentView myV3 = new ContentView()
+            {
+                BackgroundColor = Color.Black,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                WidthRequest = size,
+                HeightRequest = size
+            };
 
+            ContentView myV4 = new ContentView()
+            {
+                BackgroundColor = Color.Blue,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                WidthRequest = size,
+                HeightRequest = size
+            };
+
+            ContentView myV5 = new ContentView()
+            {
+                BackgroundColor = Color.Blue,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                WidthRequest = size,
+                HeightRequest = size
+            };
+
+            ContentView myV6 = new ContentView()
+            {
+                BackgroundColor = Color.Blue,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                WidthRequest = size,
+                HeightRequest = size
+            };
+
+            ContentView myV7 = new ContentView()
+            {
+                BackgroundColor = Color.Blue,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                WidthRequest = size,
+                HeightRequest = size
+            };
+
+
+            ContentView myV8 = new ContentView()
+            {
+                BackgroundColor = Color.Blue,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                WidthRequest = size,
+                HeightRequest = size
+            };
+
+            Children.Add(myV1, 0, 0);
+            Children.Add(myV2, 1, 0);
+            Children.Add(myV3, 2, 0);
+            Children.Add(myV4, 3, 0);
+
+            Children.Add(myV5, 0, 1);
+            Children.Add(myV6, 1, 1);
+            Children.Add(myV7, 2, 1);
+            Children.Add(myV8, 3, 1);
+
+            myV1.BackgroundColor = Color.Transparent;
+            myV2.BackgroundColor = Color.Transparent;
+            myV3.BackgroundColor = Color.Transparent;
+            myV4.BackgroundColor = Color.Transparent;
+            myV5.BackgroundColor = Color.Transparent;
+            myV6.BackgroundColor = Color.Transparent;
+            myV7.BackgroundColor = Color.Transparent;
+            myV8.BackgroundColor = Color.Transparent;
+
+            myV1.Content = GetChart();
+            myV2.Content = GetChart();
+            myV3.Content = GetChart();
+            myV4.Content = GetChart();
+            myV5.Content = GetChart();
+            myV6.Content = GetChart();
+            myV7.Content = GetChart();
+            myV8.Content = GetChart();
+
+            //Charts
             /*
             SfChart chart = new SfChart();
             chart.VerticalOptions = LayoutOptions.CenterAndExpand;
@@ -62,6 +150,18 @@ namespace RAT._1View.Desktop
 
     */
         }
+        Random rand = new Random();
+        public SfChart GetChart()
+        {
+            SfChart chart = new SfChart();
+            chart.VerticalOptions = LayoutOptions.CenterAndExpand;
+            chart.HorizontalOptions = LayoutOptions.CenterAndExpand;
+            PieSeries pieSeries = new PieSeries();
+
+            pieSeries.ItemsSource = PieSeriesData;
+            chart.Series.Add(pieSeries);
+            return chart;
+        }
 
         public ObservableCollection<ChartDataPoint> PieSeriesData
         {
@@ -69,8 +169,8 @@ namespace RAT._1View.Desktop
             {
                 //Data temp
                 ObservableCollection<ChartDataPoint> pieDatas = new ObservableCollection<ChartDataPoint>();
-                pieDatas.Add(new ChartDataPoint("54% Used", 54));
-                pieDatas.Add(new ChartDataPoint("46% Free", 46));
+                pieDatas.Add(new ChartDataPoint("54% Used", rand.Next(1, 100)));
+                pieDatas.Add(new ChartDataPoint("46% Free", rand.Next(1, 100)));
 
                 return pieDatas;
             }
